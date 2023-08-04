f7cf1a79f8a2b8a2daa87f7e2b55e229c1334cf2
manavmalhotra123 <mmalhotra_be21@thapar.edu>
2023-08-04 11:43:59 +0530

    updated now working properly

--- NumberPlate_Detection.py
+++ NumberPlate_Detection.py
@@ -2,15 +2,15 @@ import cv2
 
 frameWidth = 640
 frameHeight = 480
-nplateCascade = cv2.CascadeClassifier("Resources/haarcascade_russian_plate_number.xml")
+nplateCascade = cv2.CascadeClassifier("haarcascade_russian_plate_number.xml")
 minArea = 500
-color = (0,0,255)
+color = (0, 0, 255)
 count = 1
 
-cap = cv2.VideoCapture(1) # 0 for laptop webcam, 1 for other camera
-cap.set(3,frameWidth)
-cap.set(4,frameHeight)
-cap.set(10,500)
+cap = cv2.VideoCapture(0)  # 0 for laptop webcam, 1 for other camera
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
+cap.set(10, 150)
 
 while True:
     success, img = cap.read()
@@ -18,24 +18,27 @@ while True:
 
     numberPlates = nplateCascade.detectMultiScale(imgGray, 1.1, 4)
 
-
     for (x, y, w, h) in numberPlates:
-        area = w*h
+        area = w * h
         if area > minArea:
             cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            cv2.putText(img,"Number Plate",(x,y-5),
-                        cv2.FONT_HERSHEY_COMPLEX_SMALL,1,color,2)
-            imgROI = img[y:y+h,x:x+w]
+            cv2.putText(img, "Number Plate", (x, y - 5),
+                        cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, color, 2)
+            imgROI = img[y:y + h, x:x + w]
             cv2.imshow("ROI", imgROI)
 
-    cv2.imshow("Result",img)
-    if cv2.waitKey(1) & 0xFF == ord('p'):
-        cv2.imwrite("Resources/Scanned_Number_Plates/NoPlate_"+str(count)+".jpg",imgROI)
-        cv2.rectangle(img,(0,200),(640,300),(0,255,0),cv2.FILLED)
-        cv2.putText(img,"Scan Saved",(150,265),cv2.FONT_HERSHEY_PLAIN,
-                    2,(0,0,255),2)
-        cv2.imshow("Result",img)
+    cv2.imshow("Result", img)
+    key = cv2.waitKey(1)
+
+    if key == ord('p'):
+        cv2.imwrite("Resources/Scanned_Number_Plates/NoPlate_" + str(count) + ".jpg", imgROI)
+        cv2.rectangle(img, (0, 200), (640, 300), (0, 255, 0), cv2.FILLED)
+        cv2.putText(img, "Scan Saved", (150, 265), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 255), 2)
+        cv2.imshow("Result", img)
         cv2.waitKey(500)
         count += 1
-    elif cv2.waitKey(1) & 0xFF == ord('s'):
+    elif key == ord('s'):
         break
+
+cap.release()
+cv2.destroyAllWindows()
